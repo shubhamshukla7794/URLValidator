5ebc98d81c13f3cb8ea2b8e0e53da1fbf192bc1d
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-09-09 10:33:46 +0530

    Intial Update

---
+++ src/main/java/com/codework/urlvalidator/UrlvalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.codework.urlvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlvalidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlvalidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codework/urlvalidator/UrlvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codework.urlvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlvalidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
