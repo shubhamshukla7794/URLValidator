d4b31a4c0ed402b7ec44c7bc8909b05e2dbb4823
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-09-13 16:22:13 +0530

    Check all parents of URL as well as redirection

--- src/main/java/com/codework/urlvalidator/service/URLServiceImpl.java
+++ src/main/java/com/codework/urlvalidator/service/URLServiceImpl.java
@@ -6,7 +6,9 @@ import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 
@@ -15,6 +17,9 @@ import java.util.Optional;
 public class URLServiceImpl implements URLService {
 
     private final URLRepository urlRepository;
+    private final String SITE_IS_UP = "Site is up";
+    private final String SITE_IS_DOWN = "Site is down";
+    private final String INCORRECT_URL = "URL is incorrect";
 
     public URLServiceImpl(URLRepository urlRepository) {
         this.urlRepository = urlRepository;
@@ -24,46 +29,112 @@ public class URLServiceImpl implements URLService {
     @Override
     public URLClass findURL(String url) throws Exception {
 
-        if (urlInspector(url) == "OK") {
-            return getUrlClass(url, "OK");
+        url = getFormattedURL(url);
+
+        String status = urlInspector(url);
+
+        if (!status.equals(SITE_IS_UP) && !status.equals(SITE_IS_DOWN) && !status.equals(INCORRECT_URL)) {
+            url = status;
+            status = urlInspector(url);
+        }
+
+        if (status.equals(SITE_IS_UP)) {
+            return getSavedURL(url, status);
         } else {
-            String parentUrl = getParentUrl(url);
-            if (urlInspector(parentUrl) == "OK") {
-                return getUrlClass(parentUrl, "OK");
-            } else {
-                return getUrlClass(url,"404");
+            URLClass temp=null;
+            String parentURL=url;
+
+            while (!parentURL.equals("/") && !status.equals(SITE_IS_UP)) {
+
+                status = urlInspector(parentURL);
+                if (status.equals(SITE_IS_UP)){
+                    return getSavedURL(parentURL,status);
+                }else {
+                    temp = getSavedURL(parentURL,status);
+                }
+                parentURL = getParentUrl(parentURL);
             }
+
+            if (parentURL.equals("/")) {
+                return getUrlClassByURL(url);
+            }else {
+                return temp;
+            }
+
         }
 
     }
 
-    private String urlInspector(String url) throws Exception{
-        URL inspectURL = new URL(url);
-//        HttpURLConnection.setFollowRedirects(false);
-        HttpURLConnection connection = (HttpURLConnection) inspectURL.openConnection();
-        connection.setInstanceFollowRedirects(false);
+    private URLClass getUrlClassByURL(String url) throws Exception {
+        Optional<URLClass> urlClassOptional = urlRepository.findByCheckURL(url);
 
-        int responseCode = connection.getResponseCode();
-
-        if (responseCode == HttpURLConnection.HTTP_OK){
-            return "OK";
-        } else{
-            return "404";
+        if (urlClassOptional.isEmpty()) {
+            throw new NotFoundException("URL Not Found - " + url);
         }
+
+        return urlClassOptional.get();
+    }
+
+    private String getFormattedURL(String url) {
+        String headerUrl = "https://";
+
+        if (!url.endsWith("/")){
+            url = url.concat("/");
+        }
+
+        if (!url.startsWith("https://")) {
+            url = headerUrl.concat(url);
+        }else if (!url.startsWith("http://") && !url.startsWith("https://")){
+            url = headerUrl.concat(url);
+        }
+        return url;
+    }
+
+    private String urlInspector(String url){
+
+        try {
+            URL inspectURL = new URL(url);
+//        HttpURLConnection.setFollowRedirects(false);
+            HttpURLConnection connection = (HttpURLConnection) inspectURL.openConnection();
+            connection.setInstanceFollowRedirects(false);
+
+            int responseCode = (connection.getResponseCode())/100;
+
+            if (responseCode == 2){
+                return SITE_IS_UP;
+            } else if (responseCode == 3) {
+                return connection.getHeaderField("Location");
+            }else{
+                return SITE_IS_DOWN;
+            }
+
+        } catch (MalformedURLException e) {
+            return INCORRECT_URL;
+        } catch (IOException e) {
+            return SITE_IS_DOWN;
+        }
+
+
+
     }
 
     private String getParentUrl(String childUrl){
+        String tempChild = null;
         if (childUrl.endsWith("/")){
             childUrl = childUrl.substring(0,childUrl.length()-1);
         }
         int index = childUrl.lastIndexOf("/");
         if (index > 0) {
-            return childUrl.substring(0, index);
+            tempChild = childUrl.substring(0, index+1);
         }
-        return "/";
+
+        if (tempChild.equals("https://")  || tempChild.equals("http://"))
+            return "/";
+        else 
+            return tempChild;
     }
 
-    private URLClass getUrlClass(String url, String status) {
+    private URLClass getSavedURL(String url, String status) {
         URLClass savedURL;
         log.debug("I am in Service");
         Optional<URLClass> urlClassOptional = urlRepository.findByCheckURL(url);
@@ -96,8 +167,6 @@ public class URLServiceImpl implements URLService {
             throw new NotFoundException("Not Found"+id.toString());
         }
 
-        URLClass urlClass = urlClassOptional.get();
-
-        return urlClass;
+        return urlClassOptional.get();
     }
 }
