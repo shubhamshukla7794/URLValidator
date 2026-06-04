797aa1be621254636f4d403ac70ca89e626beb7e
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2026-06-04 15:49:40 +0530

    upgrade UI to Tailwind and fix framework 404 error routing

--- src/main/java/com/codework/urlvalidator/controller/ControllerExceptionHandler.java
+++ src/main/java/com/codework/urlvalidator/controller/ControllerExceptionHandler.java
@@ -1,19 +1,19 @@
 package com.codework.urlvalidator.controller;
 
-import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler({IllegalArgumentException.class, NoHandlerFoundException.class})
     public ModelAndView handleNotFound(Exception exception) {
         return genericExceptionMethod(exception, "404err");
     }
